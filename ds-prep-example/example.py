2d796dbed94f8a707eb1f8cb250e14bf2235d197
seancwilliams <sean@thesaucela.com>
2023-09-28 02:28:16 +0000

    Completed prep-example exercise.

---
+++ ds-prep-example/example.py
@@ -0,0 +1,17 @@
+print("""
+  ===============
+  ds-prep-example
+  ===============
+""")
+
+print("I would like to analyze {} to learn/predict {}.\n".format(
+
+   #write what you would like to analyze in the quotes here 👇
+
+  "market opening range price data",
+
+   #write what you would like to learn from that analysis here 👇
+
+  "whether the range for the rest of the day is > 1%"
+
+))
